319e558d4d10de3723eb604d4ff05925a9a8129e
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-18 15:53:32 +0900

    generated doses controller with destroy and new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
   resources :cocktails, only: [:create, :index, :new, :show] do
