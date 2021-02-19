1fce4999a5c74cc8d9dff9dd810dfa5a4db30e74
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-19 16:54:17 +0900

    search done

--- app/javascript/packs/index_search.js
+++ app/javascript/packs/index_search.js
@@ -3,14 +3,14 @@ const results = document.querySelector('.list-group');
 // add the event listener
 searchBox.addEventListener('keyup', (event) => {
   event.preventDefault();
-  const httpRequest = `https://wagon-dictionary.herokuapp.com/autocomplete/${searchBox.value}`;
+  const httpRequest = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchBox.value}`;
   fetch(httpRequest)
     .then(response => response.json())
     .then((data) => {
       results.innerHTML = "";
-      const topTen = data.words.slice(0, 4);
-      topTen.forEach((word) => {
-        results.insertAdjacentHTML("beforeend", `<a https://www.diffordsguide.com/cocktails/search?keyword%5B%5D=${word}" class="list-group-item list-group-item-action">${word}</a>`);
+      const topFive = data.drinks.slice(0, 4);
+      topFive.forEach((drink) => {
+        results.insertAdjacentHTML("beforeend", `<a href="https://makemeacocktail.com/recipes/s/?s=${drink.strDrink}" class="list-group-item list-group-item-action">${drink.strDrink}</a>`);
       });
     });
 });
