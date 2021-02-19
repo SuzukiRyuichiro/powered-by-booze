633cceeb48e7d70480808fd70e96bfe0394cbf64
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-19 13:25:13 +0900

    now the adding picture is working

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,7 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   has_many :reviews
+  has_one_attached :photo
 
   validates :name, presence: true
   # validates :imageURL, format: { with: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}(\/[-a-zA-Z0-9@:%._\+~#=]*)*/i, message: "please enter URL in correct format"}
