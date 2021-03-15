b800fdce002f3a4e60e69edb472482ad37598176
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-03-15 23:41:32 +0900

    made search method inside of the Cocktail model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -6,4 +6,14 @@ class Cocktail < ApplicationRecord
 
   validates :name, presence: true
   # validates :imageURL, format: { with: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}(\/[-a-zA-Z0-9@:%._\+~#=]*)*/i, message: "please enter URL in correct format"}
+
+  include PgSearch::Model
+   pg_search_scope :search_by_name_instruction_and_ingredients,
+     against: [ :name, :instruction ],
+     associated_against: {
+      ingredients: [ :name ]
+    },
+     using: {
+       tsearch: { prefix: true }
+     }
 end
