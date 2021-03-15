c96e43986240e7dbcff98e3484df5395f36164c7
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-03-16 01:21:57 +0900

    autofill now searches through name, instructions and ingredients

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,9 +1,29 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "results" ]
+  static targets = [ "results", "input" ]
 
   connect() {
-    this.resultsTarget.insertAdjacentHTML('beforeend', '<h1>Hello</h1>');
+
+  }
+
+  fetchResults() {
+    if (this.inputTarget.value === ""){
+      this.resultsTarget.innerHTML = "";
+    } else {
+      fetch(`/api/v1/search/${this.inputTarget.value}`)
+      .then(response => response.json())
+      .then((data) => {
+        if (data.length === 0){
+          this.resultsTarget.innerHTML = "";
+        } else {
+          this.resultsTarget.innerHTML = "";
+          data.forEach((cocktail) => {
+            const list = `<a href="/cocktails/${cocktail.id}" class="list-group-item list-group-item-action-active">${cocktail.name}</a>`;
+            this.resultsTarget.insertAdjacentHTML("beforeend", list);
+        })
+        }
+      })
+    }
   }
 }
